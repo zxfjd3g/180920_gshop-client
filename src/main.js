56ddf9a2ebdd2b7ada6f3cb0ee6d1ab56c7766dc
zxfjd3g <258147149@qq.com>
2019-02-22 16:56:55 +0800

    component

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Header from './components/Header/Header.vue'
+
+// 注册全局组件
+Vue.component('Header', Header)
 
 new Vue({
   el: '#app',
--- src/pages/Order/Order.vue
+++ src/pages/Order/Order.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>Order</div>
+  <section class="order">
+    <Header title="订单"/>
+    <section class="order_no_login">
+      <img src="./images/person.png">
+      <h3>登录后查看外卖订单</h3>
+      <button>立即登陆</button>
+    </section>
+  </section>
 </template>
 
 <script>
@@ -8,4 +15,29 @@
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
 
+  .order  //订单
+    width 100%
+
+    .order_no_login
+      padding-top 140px
+      width 60%
+      margin 0 auto
+      text-align center
+      >img
+        display block
+        width 100%
+        height 30%
+      >h3
+        padding 10px 0
+        font-size 17px
+        color #6a6a6a
+      >button
+        display inline-block
+        background #02a774
+        font-size 14px
+        color #fff
+        border 0
+        outline none
+        border-radius 5px
+        padding 10px 20px
 </style>
