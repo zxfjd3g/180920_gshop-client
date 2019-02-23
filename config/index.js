978d7a918b8f4b2cd827f977391da996ec519402
zxfjd3g <258147149@qq.com>
2019-02-23 10:37:16 +0800

    ajax

--- config/index.js
+++ config/index.js
@@ -10,7 +10,22 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': { // 匹配所有以 '/api'开头的请求路径
+        target: 'http://localhost:5000', // 代理目标的基础路径
+        changeOrigin: true, // 支持跨域
+        pathRewrite: {// 重写路径: 去掉路径中开头的'/api'
+          '^/api': ''
+        }
+      },
+      '/baidu': { // 匹配所有以 '/api'开头的请求路径
+        target: 'http://www.baidu.com', // 代理目标的基础路径
+        changeOrigin: true, // 支持跨域
+        pathRewrite: {// 重写路径: 去掉路径中开头的'/api'
+          '^/baidu': ''
+        }
+      },
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,14 @@
 
 <script>
   import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+  import {reqAddress} from './api'
   export default {
+
+    async mounted () {
+      const result = await reqAddress('116.36867', '40.10038')
+      console.log('result', result)
+    },
+
     components: {
       FooterGuide
     }
